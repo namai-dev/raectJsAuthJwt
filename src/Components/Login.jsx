0870f93dcbc1c2d5cc8734ccce02532307a75f27
namai-dev <113511617+namai-dev@users.noreply.github.com>
2023-01-12 08:29:14 +0300

    working on posting data

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,7 +3,41 @@ import Logo from "./avator.png";
 import { Helmet } from "react-helmet";
 import { Fragment } from "react";
 import Header from "./Header";
+import { useState } from "react";
+import axios from "axios";
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const url = "http://127.0.0.1:9090/login";
+
+  const check = (e) => {
+    e.preventDefault();
+    const data = {
+      username: username,
+      password: password,
+    };
+
+    console.log(data);
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    const data = {
+      username: username,
+      password: password,
+    };
+    console.log(username);
+    console.log(password);
+    const res = axios
+      .post(url, JSON.stringify({ username: username, password: password }), {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      })
+      // .post(url, {}, { params: { data } })
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <Fragment>
       <Header />
@@ -21,9 +55,26 @@ const Login = () => {
         </div>
 
         <div className="container">
-          <input className="eml" type="text" placeholder="Enter Email" />
-          <input className="psw" type="password" placeholder="Enter password" />
-          <button className="btny">Login</button>
+          <input
+            className="eml"
+            type="email"
+            placeholder="Enter Email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="psw"
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={login} className="btny">
+            Login
+          </button>
+          <button className="btny" onClick={check}>
+            Check
+          </button>
         </div>
       </div>
     </Fragment>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,11 +1,64 @@
 import React, { Fragment, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Header from "./Header";
 import Logo from "./avator.png";
 import { Helmet } from "react-helmet";
+import axios from "axios";
+// 127.0.0.1:9090/api/amazon/register
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [roles, setRoles] = useState([]);
+  const history = useNavigate();
+  const url = "http://127.0.0.1:9090/api/amazon/register";
+
+  // const onSubmit = (e) => {
+  //   const url = "http://127.0.0.1:9090/api/amazon/register";
+  //   e.preventDefault();
+
+  //   const data = {
+  //     username: username,
+  //     email: email,
+  //     password: password,
+  //     role: roles,
+  //   };
+  //   axios
+  //     .post(url, data)
+  //     .then((response) => console.log(response), history("/login"))
+  //     .catch((err) => console.log(err));
+  // };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      username: username,
+      email: email,
+      password: password,
+      role: roles,
+    };
+
+    try {
+      const responce = axios
+        .post(url, data)
+        .then((response) => {
+          console.log(response.data);
+          history("/login");
+          alert(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      // console.log(responce.data);
+      // history("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Fragment>
       <Header />
@@ -23,10 +76,30 @@ const Register = () => {
         </div>
 
         <div className="container">
-          <input className="eml" type="text" placeholder="Enter Username" />
-          <input className="eml" type="text" placeholder="Enter Email" />
-          <input className="psw" type="password" placeholder="Enter password" />
-          <button className="btny">Register</button>
+          <input
+            className="eml"
+            type="text"
+            placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="eml"
+            type="text"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="psw"
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="btny" onClick={onSubmit}>
+            Register
+          </button>
           <span>
             Already have an account <a href="">Login</a>
           </span>
